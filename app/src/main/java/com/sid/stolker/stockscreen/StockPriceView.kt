d89f52b31713afc38cd261aa0be8eefe4cc96be5
Siddhant <siddhant.gupta@dailyobjects.com>
2018-04-21 16:18:26 +0530

    added view stub

--- app/src/main/java/com/sid/stolker/stockscreen/StockPriceView.kt
+++ app/src/main/java/com/sid/stolker/stockscreen/StockPriceView.kt
@@ -1,14 +1,19 @@
 package com.sid.stolker.stockscreen
 
 import android.arch.lifecycle.Observer
-import android.util.Log
 import android.view.View
 import kotlinx.android.extensions.LayoutContainer
+import kotlinx.android.synthetic.main.content_ticker.*
 
 class StockPriceView(override val containerView: View?) : Observer<StockPriceViewData>,
         LayoutContainer {
 
     override fun onChanged(viewData: StockPriceViewData?) {
-        Log.d("viewData", viewData?.toString())
+        if (viewData == null) return
+        tv_closing_price.text = "Closing Price : " + viewData.closingPrice
+        tv_current_price.text = "Current Price : " + viewData.currentPrice
+        tv_highest_price.text = "Highest Price : " + viewData.highestPrice
+        tv_lowest_price.text = "Lowest Price : " + viewData.lowestPrice
+        tv_open_price.text = "Opening Price : " + viewData.openPrice
     }
 }
